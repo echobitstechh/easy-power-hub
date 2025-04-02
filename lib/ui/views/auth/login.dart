4817b46429e046f7fbdfd79c5f52792991edd2ce
George David <georgequin19@gmail.com>
2025-04-02 12:15:18 +0100

    little fixes around the system, cards, local storage, cart, services.

--- lib/ui/views/auth/login.dart
+++ lib/ui/views/auth/login.dart
@@ -231,18 +231,51 @@ class _LoginState extends State<Login> {
                 ],
               ),
               verticalSpaceMedium,
-              SubmitButton(
-                isLoading: model.isBusy,
-                boldText: true,
-                iconIsPrefix: true,
-                icon: FontAwesomeIcons.google,
-                label: "Sign in with Google",
-                textColor: Colors.black,
-                submit:() {
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white, // Text color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 2, // Subtle shadow
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  minimumSize: const Size(double.infinity, 54), // Full-width button
+                ),
+                onPressed: () {
                   model.signInWithGoogle(context);
                 },
-                color: Colors.grey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/crypto%2Fsearch%20(2).png?alt=media&token=24a918f7-3564-4290-b7e4-08ff54b3c94c",
+                      width: 20,
+                    ),
+                    const SizedBox(width: 20),
+                    const Text(
+                      'Sign in with Google',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              // SubmitButton(
+              //   isLoading: model.isBusy,
+              //   boldText: true,
+              //   iconIsPrefix: true,
+              //   icon: FontAwesomeIcons.google,
+              //   label: "Sign in with Google",
+              //   textColor: Colors.black,
+              //   submit:() {
+              //     model.signInWithGoogle(context);
+              //   },
+              //   color: Colors.grey,
+              // ),
               verticalSpaceMedium,
               Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                 const Text(
