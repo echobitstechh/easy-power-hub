85f97e7871e987cf6968e503692634df1c2fa2d8
Yehhmii <franciscoabhulimen@gmail.com>
2025-10-31 15:32:08 -0600

    review update

--- lib/features/Profile/referral/referrals_viewmodel.dart
+++ lib/features/Profile/referral/referrals_viewmodel.dart
@@ -62,7 +62,7 @@ class ReferralsViewModel extends BaseViewModel {
     }
     
     Share.share(
-      'Join me on EasyOpenHub and get amazing products! 🎁\n\n'
+      'Join me on EasyPowerHub and get amazing products! 🎁\n\n'
       'Use my referral code: $referralCode\n'
       'Or click this link: $referralLink\n\n'
       'Sign up now and enjoy exclusive deals!',
